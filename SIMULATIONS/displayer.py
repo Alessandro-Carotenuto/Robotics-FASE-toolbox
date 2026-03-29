1da155d731263a2b78d4fa5edd72fd8ae36719d2
Alessandro Carotenuto <alex1499@gmail.com>
2026-03-29 03:04:35 +0200

    Animated Simulation

--- SIMULATIONS/displayer.py
+++ SIMULATIONS/displayer.py
@@ -34,3 +34,38 @@ class Displayer():
 
         plt.tight_layout()
         plt.show()
+    
+    def animate_display(self, Title="Animated Simulation",xlabel="X",ylabel="Y", fixed_view=False):
+
+        fig, ax = plt.subplots(figsize=(6, 6))
+        plt.ion()
+                               
+        for step in range(len(self.env.trajectories[id(self.env.robots[0])])):
+            ax.clear()
+            i=0
+            for robot in self.env.robots:
+                currentcolor=self.colorlist[i]
+                trajectory=self.env.trajectories[id(robot)]
+                trajectory = np.array(trajectory)
+                
+                ax.plot(trajectory[:step, 0], trajectory[:step, 1], color=currentcolor)
+
+                if (i==len(self.colorlist)-1):
+                    i=0
+                else:
+                    i+=1
+        
+
+            ax.set_title(Title)
+            ax.set_xlabel(xlabel)
+            ax.set_ylabel(ylabel)
+            ax.axis("equal")
+            ax.grid(True)
+            plt.tight_layout()
+            plt.pause(0.01)
+
+        plt.ioff()
+        plt.show()
+
+
+    
